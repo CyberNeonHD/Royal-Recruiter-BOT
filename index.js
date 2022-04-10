b8d155f824f928cd533fa9613cf396f160d1576a
Kmotion <Kobbe12@hotmail.com>
2022-04-10 22:40:00 +0200

    process.env for discord token

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const { Client, Collection } = require('discord.js');
 const DiscordBot = new Client({intents:32767})
-const { Token } = require('./config.json')
 
 require("./Handlers/Events")(DiscordBot);
 require("./Handlers/Commands")(DiscordBot);
 
 DiscordBot.commands = new Collection();
 
-DiscordBot.login(Token);
+DiscordBot.login(process.env.DSJ_Token);
