cc08bf7ad3ed6dd9349e6f3367cacec4b35bde81
Kmotion <Kobbe12@hotmail.com>
2022-04-10 22:34:46 +0200

    heroku Procfile

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, Collection } = require('discord.js');
 const DiscordBot = new Client({intents:32767})
 const { Token } = require('./config.json')
+
 require("./Handlers/Events")(DiscordBot);
 require("./Handlers/Commands")(DiscordBot);
 
