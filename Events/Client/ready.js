23e7d7aa293f9b931fc28cbcddd4e39aeaf4266b
kobbe.vereecken <kobbe.vereecken@student.howest.be>
2022-07-01 00:47:34 +0200

    code tweaking

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -5,16 +5,18 @@ module.exports = {
     once: true,
 
    execute(DiscordBot) {
-       DiscordBot.user.setActivity('Recruitment', {type: "WATCHING"});
+        DiscordBot.user.setActivity('Recruitment', {type: "WATCHING"});
 
-       //if(!process.env.Database) return;
-       mongoose.connect('mongodb+srv://Cyberneon:AnonMongoKmotion258@royalrecruiter.xgpw0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+        if(!process.env.Database){
+            return;
+        }
+       mongoose.connect(process.env.Database, {
            useNewUrlParser: true,
            useUnifiedTopology: true
        }).then(() => {
-           console.log('The client is now connected to the database.')
+           console.log('The client is now connected to the database.');
        }).catch((err) =>{
-           console.log(err)
-       })
+           console.log(err);
+       });
    }
-}
+};
--- Events/Interaction/interaction.js
+++ Events/Interaction/interaction.js
@@ -3,21 +3,25 @@ const {Client, MessageEmbed, CommandInteraction} = require('discord.js');
 module.exports = {
     name: 'interactionCreate',
     /**
-     * 
-     * @param {CommandInteraction} interaction 
-     * @param {Client} DiscordBot 
+     *
+     * @param {CommandInteraction} interaction
+     * @param {Client} DiscordBot
      */
     async execute(interaction, DiscordBot) {
         if(interaction.isCommand()) {
-            const command = DiscordBot.commands.get(interaction.commandName)
-            if(!command) return interaction.reply({embeds: [
-                new MessageEmbed()
-                .setColor('RED')
-                .setDescription('🚩 An error occured while handling this command.')
-            ]}) && DiscordBot.commands.delete(interaction.commandName);
 
-            command.execute(interaction, DiscordBot)
+            const command = DiscordBot.commands.get(interaction.commandName);
+            if(!command) {
+                return interaction.reply({
+                    embeds: [
+                        new MessageEmbed()
+                        .setColor('RED')
+                        .setDescription('🚩 An error occured while handling this command.')
+                    ]
+                }) && DiscordBot.commands.delete(interaction.commandName);
+            }
+
+            command.execute(interaction, DiscordBot);
         }
-
     }
-}
+};
