cf89175abacdc5c464e79a1eb7c421694927c530
kobbe.vereecken <kobbe.vereecken@student.howest.be>
2022-07-01 23:54:08 +0200

    Update roleManagerProspect.js

--- Commands/Recruiters/roleManagerProspect.js
+++ Commands/Recruiters/roleManagerProspect.js
@@ -33,11 +33,33 @@ module.exports = {
             embed.setDescription("User has that role already??");
         }
         else {
-            target.roles.add(role);
             const nameOfTarget = target.displayName;
+            target.roles.add(role);
             target.setNickname("P | " + nameOfTarget);
-            embed.setDescription(`Added the ${role} role to ${target}.`);
+
+            sendWelcomeMessageProspect(interaction, target);
+            embed.setDescription(`Prospect welcome message is posted.\nI also added the ${role} role to ${target}.`);
         }
         await interaction.reply({embeds: [embed], fetchReply: true});
     }
 };
+
+function sendWelcomeMessageProspect(interaction, target) {
+    const channelProspects = interaction.guild.channels.cache.get("956199691282944042");
+    const expectationsChannel = interaction.guild.channels.cache.get('949717343934750800').toString();
+    const meetMentorChannel = interaction.guild.channels.cache.get('949717343934750800').toString();
+    const meetProspectChannel = interaction.guild.channels.cache.get('949717343934750800').toString();
+    const goingAwayChannel = interaction.guild.channels.cache.get('949717343934750800').toString();
+    const processFeedbackChannel = interaction.guild.channels.cache.get('949717343934750800').toString();
+
+    channelProspects.send(
+`@here
+Welcome ${target}
+When you have time. Check out ${expectationsChannel}.
+Get to know your mentor and see your fellow teammates in ${meetMentorChannel}.
+Give us a little info on yourself in ${meetProspectChannel} (whatever you are comfortable with sharing).
+Let us know if you need to be put on hold or will be less active in ${goingAwayChannel}.
+If you're feeling nice, shoot us a message in ${processFeedbackChannel}. We want to know how your experience is with us 🙂.
+`);
+
+}
