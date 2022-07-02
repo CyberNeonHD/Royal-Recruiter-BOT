1a9a87cfeedcd0838fc23d0fa0ce54634bc3ef3e
kobbe.vereecken <kobbe.vereecken@student.howest.be>
2022-07-02 03:39:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ DiscordBot.commands = new Collection();
 require("./Handlers/Events")(DiscordBot);
 require("./Handlers/Commands")(DiscordBot);
 
-DiscordBot.login("OTU1NzUxMTk4NjMwMjQ0Mzcy.YjmOqg.mViYcoDMnHNOfxQo9NRYtuJl55I");
+DiscordBot.login(process.env.DSJ_Token);
