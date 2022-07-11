006c77d04ccec5b07f461b74653c98ca3fadcf05
kobbe.vereecken <kobbe.vereecken@student.howest.be>
2022-07-11 23:52:45 +0200

    Update voting.js

--- Commands/server/voting.js
+++ Commands/server/voting.js
@@ -12,8 +12,8 @@ module.exports = {
         const roleRB = interaction.guild.roles.cache.get("528574587747958794");
         const Response = new MessageEmbed();
         Response.setColor('BLUE');
-        Response.setDescription(`React with <:VOTEYES:843159948317163570> or <:VOTENO:843159948090933288>.\n Or ❔ if you havent met them/played with them yet.
-        Please write the reason you have put this answer on the thread.`);
+        Response.setDescription(`React with <:VOTEYES:843159948317163570> or <:VOTENO:843159948090933288>.\nOr ❔ if you havent met them/played with them yet.
+        \nPlease write the reason you have put this answer on the thread.`);
         const message = await interaction.reply({embeds: [Response], fetchReply: true });
         message.react('<:VOTEYES:843159948317163570>');
         message.react('<:VOTENO:843159948090933288>');
