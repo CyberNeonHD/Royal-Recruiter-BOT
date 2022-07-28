0eddcb0bcb6aa2d56b2c544e68defb89dedb4de8
Magnew-98 <markosagnew@gmail.com>
2022-07-28 19:26:11 +0100

    Revert "Added rbcadet command"
    This reverts commit 2e62558cedd80aa25c57580d991f6632a6f9d411.

--- Commands/Developer/patchnotes.js
+++ Commands/Developer/patchnotes.js
@@ -14,8 +14,8 @@ module.exports = {
         Response.setColor('#0099ff');
         Response.setTitle('Patch Notes');
         Response.setThumbnail('https://i.imgur.com/0zHd6L9.png');
-        Response.setDescription(`- added new for /rbcadet command.`)
-        .setFooter({ text: 'Last updated and tested on 28/07/2022', iconURL: 'https://i.imgur.com/0zHd6L9.png' });
+        Response.setDescription(`- Extra message of prospects tickets added for /rbmember command.`)
+        .setFooter({ text: 'Last updated and tested on 26/07/2022', iconURL: 'https://i.imgur.com/0zHd6L9.png' });
 
         interaction.reply({embeds: [Response], fetchReply: true});
     }
