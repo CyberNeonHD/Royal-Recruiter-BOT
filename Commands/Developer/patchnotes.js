9e5af4cc4ea50302b7da7175644d143dcdd861ab
Magnew-98 <markosagnew@gmail.com>
2022-07-28 18:47:12 +0100

    added react for assigning prospect to mentor

--- Commands/Developer/patchnotes.js
+++ Commands/Developer/patchnotes.js
@@ -14,7 +14,7 @@ module.exports = {
         Response.setColor('#0099ff');
         Response.setTitle('Patch Notes');
         Response.setThumbnail('https://i.imgur.com/0zHd6L9.png');
-        Response.setDescription(`- added new for /rbcadet command.`)
+        Response.setDescription(`- added reacting to claim prospect as mentee`)
         .setFooter({ text: 'Last updated and tested on 28/07/2022', iconURL: 'https://i.imgur.com/0zHd6L9.png' });
 
         interaction.reply({embeds: [Response], fetchReply: true});
