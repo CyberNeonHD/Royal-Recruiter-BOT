db364649f3cccd5f441e8ca7ae64fa2e7c8f50bb
Magnew-98 <markosagnew@gmail.com>
2022-08-23 23:17:17 +0100

    testing

--- Commands/Recruiters/BattlemetricsStats.js
+++ Commands/Recruiters/BattlemetricsStats.js
@@ -2,7 +2,7 @@ const { MessageEmbed, CommandInteraction } = require('discord.js');
 require('../../Events/Client/ready');
 const BM = require('@leventhan/battlemetrics');
 const bmoptions = {
-    token: process.env.TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjI4MzVmMGFiNTZhY2ViZjAiLCJpYXQiOjE2NjExMjI2MDEsIm5iZiI6MTY2MTEyMjYwMSwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo1NDg1MTgifQ.yti2ZP6VAf6GQX5clSIby5kWFpd_mETryupDYarb4r4", // after v1.4.8 don't add Bearer!
+    token: process.env.TOKEN || "eyJhbGcieyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjA5OGUzZDA0YmY5OGY4M2YiLCJpYXQiOjE2NjEyOTI4MDIsIm5iZiI6MTY2MTI5MjgwMiwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo1NDg1MTgifQ.TAR4EDRXFssH2Og2aU6hJoO-7C33gSYrQLR5p1x0KfwOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjI4MzVmMGFiNTZhY2ViZjAiLCJpYXQiOjE2NjExMjI2MDEsIm5iZiI6MTY2MTEyMjYwMSwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo1NDg1MTgifQ.yti2ZP6VAf6GQX5clSIby5kWFpd_mETryupDYarb4r4", // after v1.4.8 don't add Bearer!
     serverID: process.env.SerVER_ID || '4750113',
     game: process.env.GAME || 'squad'
 };
@@ -30,11 +30,9 @@ module.exports = {
         const { options } = interaction;
         const steam64id = options.getString('steam64id');
 
-        const awaitExample = await tBM.getPlayerInfoBy(
-            "steamID", `${steam64id}`
-        );
+
         
-        awaitExample.then((value) => {
+        tBM.getPlayerInfoBy("steamID", `${steam64id}`).then((res) => {
             const message = "Get players info by searching on identifier.";
             interaction.reply("=".repeat(message.length));
             interaction.reply(`${message}`);
@@ -48,7 +46,7 @@ module.exports = {
         const Response = new MessageEmbed()
         .setColor('BLUE')
         .setTitle('testing')
-        .setDescription(`**steam64id saved**: ${message}\n`)
+        .setDescription(`**steam64id saved**\n`)
 
 
         interaction.reply({embeds: [Response], fetchReply: true});
