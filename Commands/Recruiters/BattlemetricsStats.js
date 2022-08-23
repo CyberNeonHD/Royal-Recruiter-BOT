a52897ab8a3e26a7344cc8b22ae93f36bfab4750
Magnew-98 <markosagnew@gmail.com>
2022-08-24 00:10:55 +0100

    fixed possible typo

--- Commands/Recruiters/BattlemetricsStats.js
+++ Commands/Recruiters/BattlemetricsStats.js
@@ -3,7 +3,7 @@ require('../../Events/Client/ready');
 const BM = require('@leventhan/battlemetrics');
 const bmoptions = {
     token: process.env.TOKEN || "eyJhbGcieyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjA5OGUzZDA0YmY5OGY4M2YiLCJpYXQiOjE2NjEyOTI4MDIsIm5iZiI6MTY2MTI5MjgwMiwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo1NDg1MTgifQ.TAR4EDRXFssH2Og2aU6hJoO-7C33gSYrQLR5p1x0KfwOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjI4MzVmMGFiNTZhY2ViZjAiLCJpYXQiOjE2NjExMjI2MDEsIm5iZiI6MTY2MTEyMjYwMSwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo1NDg1MTgifQ.yti2ZP6VAf6GQX5clSIby5kWFpd_mETryupDYarb4r4", // after v1.4.8 don't add Bearer!
-    serverID: process.env.SerVER_ID || '4750113',
+    serverID: process.env.SERVER_ID || '4750113',
     game: process.env.GAME || 'squad'
 };
 const tBM = new BM(bmoptions);
