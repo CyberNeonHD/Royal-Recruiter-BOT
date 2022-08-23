7fc501473899dcfc630f63f6b2822b117344d338
Magnew-98 <markosagnew@gmail.com>
2022-08-23 17:35:59 +0100

    added api to test

--- Commands/Recruiters/BattlemetricsStats.js
+++ Commands/Recruiters/BattlemetricsStats.js
@@ -1,5 +1,12 @@
 const { MessageEmbed, CommandInteraction } = require('discord.js');
 require('../../Events/Client/ready');
+const BM = require('@leventhan/battlemetrics');
+const bmoptions = {
+    token: process.env.TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjI4MzVmMGFiNTZhY2ViZjAiLCJpYXQiOjE2NjExMjI2MDEsIm5iZiI6MTY2MTEyMjYwMSwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo1NDg1MTgifQ.yti2ZP6VAf6GQX5clSIby5kWFpd_mETryupDYarb4r4", // after v1.4.8 don't add Bearer!
+    serverID: process.env.SerVER_ID || '4750113',
+    game: process.env.GAME || 'squad'
+};
+const tBM = new BM(bmoptions);
 
 module.exports = {
     name: 'bmstats',
